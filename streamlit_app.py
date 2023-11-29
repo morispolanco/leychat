e6846f63a9517b7f806c80d153d8aff86898b529
morispolanco <morispolanco@gmail.com>
2023-11-29 03:00:21 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,25 @@
 import streamlit as st
-import requests 
+import requests
 
-def call_api(message):
+def call_api():
     url = "https://api.afforai.com/api/api_completion"
+    headers = {
+        "Content-Type": "application/json"
+    }
     payload = {
         "apiKey": "fcbfdfe8-e9ed-41f3-a7d8-b6587538e84e",
         "sessionID": "65489d7c9ad727940f2ab26f",
-        "history": [{"role": "user", "content": message}],
+        "history": [
+            {
+                "role": "user",
+                "content": "What is AI?"
+            }
+        ],
         "powerful": False,
         "google": True
     }
-    response = requests.post(url, json=payload)
+    response = requests.post(url, json=payload, headers=headers)
     return response.json()
 
-def main():
-    st.title("Chatbot de IA")
-    
-    user_input = st.text_input("Usuario:", value="What is AI?")
-    if st.button("Enviar"):
-        response = call_api(user_input)
-        st.markdown(f"**Bot:** {response['suggestedUserResponses'][0]}")
-
-if __name__ == "__main__":
-    main()
+response = call_api()
+st.write(response)
